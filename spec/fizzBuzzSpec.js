a7e5b2906a9b321d4bc5b163eae5ceff1073f594
Kate102 <student@Admins-MacBook-Pro-4.local>
2019-03-05 14:15:44 +0000

    Failing jasmine

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,11 @@
+describe('FizzBuzz', function() {
+
+  var fizzbuzz;
+
+  describe('it knows when a number is... ',function() {
+    it('divisible by 3', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(isDivisibleByThree(3)).toBe(true);
+    };
+  });
+};
