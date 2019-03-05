16437536c15b9348939fc5948382afc43ef478bf
James Ault <james.ault@johnlewis.co.uk>
2019-03-05 14:39:23 +0000

    Added tests for modulus 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -7,5 +7,9 @@ describe('FizzBuzz', function() {
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
+    it('divisible by 5', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
   });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,8 +1,18 @@
 function FizzBuzz() {
 
 }
+
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
   if ( number % 3 == 0 ){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+FizzBuzz.prototype.isDivisibleByFive = function (number) {
+  if ( number % 5 == 0 ){
     return true;
   }
   else {
